72200d2f27790ae1bc4b778180452ef6869eceb6
Zherchyk <66788385+zverchyk@users.noreply.github.com>
2023-04-27 23:24:49 -0600

    html done almost

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+new Swiper('.slider-body', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+    simulateTouch: true,
+    grabCursor: true,
+});
