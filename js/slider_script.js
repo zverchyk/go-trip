26cd40a81cd8b75aeee8102758c6821cf4aa5306
Zherchyk <66788385+zverchyk@users.noreply.github.com>
2023-05-23 08:59:28 -0600

    rough addptive (done)

--- js/slider_script.js
+++ js/slider_script.js
@@ -1,14 +1,25 @@
-
-new Swiper('.slider-body', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true
-    },
-    simulateTouch: true,
+var swiper = new Swiper(".slider-body", {
+    effect: "coverflow",
     grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: -20,
+      stretch: 0,
+      depth: 10,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: '.swiper-button-prev',
+        prevEl: '.swiper-button-next',
+    }, 
+ 
+  });
 
-});
+ // pagination="true" effect="coverflow" grab-cursor="true" centered-slides="true"
+   // slides-per-view="auto" coverflow-effect-rotate="50" coverflow-effect-stretch="0" coverflow-effect-depth="100"
+//   coverflow-effect-modifier="1" coverflow-effect-slide-shadows="true"///
